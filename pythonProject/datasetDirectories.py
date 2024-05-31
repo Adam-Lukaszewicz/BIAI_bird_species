e128ef661727673797e69ef208c6be5bfd7fd1a6
Adam Łukaszewicz <jakub.adam20@gmail.com>
2024-05-31 18:39:20 +0200

    basic model

---
+++ pythonProject/datasetDirectories.py
@@ -0,0 +1,5 @@
+class DatasetDirectories:
+    def __init__(self):
+        self.trainPath = "F:/archive/train"
+        self.valPath = "F:/archive/valid"
+        self.testPath = "F:/archive/test"
---
+++ pythonProject/main.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt # For data viz
+import pandas as pd
+import numpy as np
+import sys
+from tqdm import tqdm
+
+from datasetDirectories import DatasetDirectories
+
+
+class SimpleSpeciesClassifier(nn.Module):
+    def __init__(self, num_classes = 525):
+        super(SimpleSpeciesClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+        enet_out_size = 1280
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+class BirdSpeciesDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data = ImageFolder(data_dir, transform=transform)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    @property
+    def classes(self):
+        return self.data.classes
+
+
+paths = DatasetDirectories()
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+train_dataset = BirdSpeciesDataset(data_dir=paths.trainPath, transform=transform)
+val_dataset = BirdSpeciesDataset(data_dir=paths.valPath, transform=transform)
+test_dataset = BirdSpeciesDataset(data_dir=paths.testPath, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+model = SimpleSpeciesClassifier()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 3
+train_losses, val_losses = [], []
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc="Training:"):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * labels.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    model.eval()
+    running_val_loss = 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader, desc="Validation:"):
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_val_loss += loss.item() * labels.size(0)
+    val_loss = running_val_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+
+    print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {train_loss}, Validation loss: {val_loss}")
+
+    plt.plot(train_losses, label='Training loss')
+    plt.plot(val_losses, label='Validation loss')
+    plt.legend()
+    plt.title("Loss over epochs")
+    plt.show()
